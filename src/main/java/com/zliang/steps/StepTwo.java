2af2355e0079ff6fe479589c5971ca9728a46893
phoebusliang <phoebusliang@gmail.com>
2016-09-30 23:44:07 +0800

    Update steps.

--- src/main/java/com/zliang/steps/StepTwo.java
+++ src/main/java/com/zliang/steps/StepTwo.java
@@ -1,20 +1,16 @@
 package com.zliang.steps;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 /**
  * Created by twe on 9/27/16.
  */
 public class StepTwo extends BasePage {
 
-    @Given("^Close driver$")
-    public void I_have_the_this_useless_scenario() {
-//        openUrl(arg1);
-//        basicOperation.openUrl()
-        getBasicOperation().closeWebDriverSession();
-        // :D
-//        driver.findElement()
-//        driver.get();
-//        driver.get("http://www.baidu.com");
+    @When("^Refresh")
+    public void Refresh() {
+        getBasicOperation().openUrl("http://www.google.com");
     }
+
 }
--- src/main/java/com/zliang/steps/Steps.java
+++ src/main/java/com/zliang/steps/Steps.java
@@ -1,38 +1,26 @@
 package com.zliang.steps;
 
+import com.zliang.runtimes.TestProperty;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebElement;
+
+import java.util.Objects;
+
+import static junit.framework.TestCase.assertTrue;
 
 
 public class Steps extends BasePage {
 
-//    public Steps() {
-//
-//    }
-
     @Given("^I have the this useless scenario \"(.*?)\"$")
     public void I_have_the_this_useless_scenario(String arg1) {
-//        openUrl(arg1);
         getBasicOperation().openUrl(arg1);
-        // :D
-//        driver.findElement()
-//        driver.get();
-//        driver.get("http://www.baidu.com");
-//        setScenarioProperty("abc", 1323423);
-
     }
 
-    @When("^I sleep for (\\d+) seconds$")
-    public void I_sleep_for_seconds(int arg1) throws InterruptedException {
-        Thread.sleep(arg1 * 1000);
-//        driver.findElement().click();
+    @When("I want to type message \"(.*?)\"")
+    public void I_want_to_type_message(String arg1) {
+        getBasicOperation().typeText(TestProperty.getProperty("homepage", "search"), arg1);
+        setScenarioProperty("title", "百度一下，你就知道");
     }
-
-    @Then("^It should finnish$")
-    public void It_should_finnish() {
-//        driver.findElement('button').click();
-//        abc = (String)getScenarioProperty("abc");
-    }
-
 }
